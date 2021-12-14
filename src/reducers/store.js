1422e652297286678852fe17b845e2471a8b790e
Bekfola <90773106+Bekfola@users.noreply.github.com>
2021-12-15 01:50:00 +0300

    new index and public

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -15,15 +15,9 @@ const reducer = (state = initialState, action) => {
         case 'SET_INIT_PHOTOS':
             return {...state, photos: [...state.photos, ...action.initPhotos]};
         case 'SET_ACCESS_TOKEN':
-            if (!state.accessToken) {
             return {...state, accessToken: action.accessToken};
-            }
-            else {return state}
         case 'SET_BEARER_TOKEN':
-            if (!state.bearerToken || !action.bearerToken) {return state}
-            else {
-                return {...state, bearerToken: action.bearerToken};
-                }
+            return {...state, bearerToken: action.bearerToken}; 
         case 'GET_CURRENT_PHOTO':
             return {...state, currentPhoto: state.photos.find(el => el.id == action.photoId) };
         case 'GET_LARGE_PHOTO_URL':
